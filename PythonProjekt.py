9648390f20b9da7109f4893b7b14a38524ae6f51
dinodawid <93006994+dinodawid@users.noreply.github.com>
2022-01-14 21:44:18 +0100

    Projekt książki adresowej

---
+++ PythonProjekt.py
@@ -0,0 +1,112 @@
+import json
+from os.path import exists
+
+
+class Kontakt:
+    def __init__(self, imie, nazwisko, telefon, email):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.telefon = telefon
+        self.email = email
+
+class KsiazkaAdresowa:
+    def __init__(self):
+        self._kontakty = []
+    def DodajKontakt(self, imie, nazwisko, telefon, email):
+        kontakt=Kontakt(imie,nazwisko,telefon, email)
+        self._kontakty.append(kontakt)
+    def WyswietlKontakty(self):
+        for kontakt in self._kontakty:
+            print(kontakt.imie, kontakt.nazwisko, kontakt.telefon, kontakt.email)
+
+    def EdytujKontakt(self, numer):
+        pass
+
+    def WyszukajKontakt(self, imie, nazwisko):
+        for kontakt in self._kontakty:
+            if (kontakt.imie.lower() and kontakt.nazwisko.lower()) == (imie.lower() and nazwisko.lower()):
+                print(kontakt.imie, kontakt.nazwisko, kontakt.telefon, kontakt.email)
+    def UsunKontakt(self, imie, nazwisko):
+        for kontakt in self._kontakty:
+            if (kontakt.imie.lower() and kontakt.nazwisko.lower()) == (imie.lower() and nazwisko.lower()):
+                self._kontakty.remove(kontakt)
+    def UsunKontakty(self):
+        self._kontakty=[]
+    def EksportujKontakty(self):
+        plik = 'kontakty.json'
+        exp_kon=[]
+        with open(plik, 'w') as f:
+            for kontakt in self._kontakty:
+                x=(kontakt.imie,kontakt.nazwisko,kontakt.telefon,kontakt.email)
+                exp_kon.append(x)
+            f.write(json.dumps(exp_kon))
+    def ImportujKontakty(self):
+        plik ="kontakty.json"
+        if(exists(plik)):
+            with open(plik, 'r') as f:
+                test=json.loads(f.read())
+                for x in test:
+                    imie=x[0]
+                    nazwisko=x[1]
+                    telefon=x[2]
+                    email=x[3]
+                    KsiazkaAdresowa.DodajKontakt(self,imie,nazwisko,telefon,email)
+
+
+
+
+
+
+ksiazkaAdresowa=KsiazkaAdresowa()
+while True:
+    choice=input("""\t
+    Menu książki kontaktów
+    [1] Wyświetl kontakty
+    [2] Dodaj kontakt
+    [3] Edytuj kontakty #TODO
+    [4] Wyszukaj kontakt
+    [5] Usuń kontakt
+    [6] Usuń kontakty
+    [7] Eksportuj kontakty
+    [8] Importuj kontakty
+    [9] Wyjdź 
+    > \t""")
+    if(choice=="1"):
+        ksiazkaAdresowa.WyswietlKontakty()
+    elif(choice=="2"):
+        imie=input("Wprowadz imie: ")
+        nazwisko=input("Wprowadz nazwisko: ")
+        telefon=input("Wprowadz numer telefonu: ")
+        email=input("Podaj adres email: ")
+
+        ksiazkaAdresowa.DodajKontakt(imie, nazwisko, telefon, email)
+    elif (choice == "3"):
+        numer=int(input())
+        ksiazkaAdresowa.EdytujKontakt(numer)
+    elif (choice == "4"):
+        imie=input("Podaj imie ")
+        nazwisko=input("Podaj nazwisko ")
+        ksiazkaAdresowa.WyszukajKontakt(imie, nazwisko)
+    elif (choice == "5"):
+        imie = str(input("Podaj imie "))
+        nazwisko = input("Podaj nazwisko ")
+        ksiazkaAdresowa.UsunKontakt(imie, nazwisko )
+    elif (choice == "6"):
+        print("Czy na pewno chcesz usuną wszystkie kontakty")
+        print("y/n ?")
+        x=input(">")
+        if(x.lower()=="n"):
+            continue
+        elif(x.lower()=="y"):
+            ksiazkaAdresowa.UsunKontakty()
+        else:
+            print("Nie prawidłowa wartość")
+            continue
+    elif (choice == "7"):
+        ksiazkaAdresowa.EksportujKontakty()
+    elif (choice == "8"):
+        ksiazkaAdresowa.ImportujKontakty()
+    elif (choice == "9"):
+        break
+    else:
+        continue
